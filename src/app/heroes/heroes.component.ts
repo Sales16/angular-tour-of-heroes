3ecbfe3ce63fa69599502c1044b10f604f01964f
Eduardo Sales <eduardo.sales.silva1603@gmail.com>
2025-06-05 13:29:26 -0300

    Atualiza o componente Heroes para exibir uma lista de heróis, removendo detalhes individuais e utilizando a estrutura de lista.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +10,5 @@ import { Hero } from '../hero';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-    hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
 }
